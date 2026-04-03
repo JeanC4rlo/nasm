6fa406551c9a1537b617dabfc451a6175aabe83e
JeanC4rlo <jeancs.santos1@gmail.com>
2026-04-02 22:41:55 -0300

    Envio de estudos de NASM sobre abrir e ler arquivos

---
+++ olive_stem/file/file.asm
@@ -0,0 +1,40 @@
+section .data
+    pathname dd "/home/jean/nasm/olive_stem/file/file.txt"
+
+section .bss
+    buffer resb 1024
+
+section .text
+    global main
+
+main:
+    ; open(path, flag)
+
+    ; Preparando o argumento com o caminho do arquivo
+    mov ebx, pathname
+
+    ; Preparando o argumento com a flag da operação (read-only - 0)
+    mov ecx, 0
+    
+    ; Preparando a syscall "open" (5)
+    mov eax, 5
+    int 80h
+
+    ; read(id, buffer, size)
+
+    ; Movendo o arquivo que foi lido (dado que o identifier saiu em eax)
+    mov ebx, eax
+
+    ; Preparando o argumento que contém o buffer 
+    mov ecx, buffer
+
+    ; Preparando o argumento que tem o tamanho do arquivo
+    mov edx, 1024
+
+    ; Preparando a syscall "read" (3)
+    mov eax, 3
+    int 80h
+
+    mov eax, 1
+    mov ebx, 0
+    int 80h
